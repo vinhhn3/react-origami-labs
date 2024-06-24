1bcb4bc4b050d4dc8bd5312424b6fc489726c99f
Vinh Hoang <vinhhn3@fpt.edu.vn>
2024-06-24 14:18:59 +0700

    chore: update MongoDB URI in config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,6 +3,7 @@ const env = process.env.NODE_ENV || "development";
 const config = {
   development: {
     port: process.env.PORT || 9999,
+    // Need to update your MongoDB URI here
     dbURL: "mongodb://localhost:27017/rest-api-db",
     authCookieName: "x-auth-token",
   },
