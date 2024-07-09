aab08837fee408807b49fa24df6b373e243b1969
Vinh Hoang <vinhhn3@fpt.edu.vn>
2024-07-09 08:32:08 +0700

    feat: add new action types for user login/logout and fetching posts

--- frontend/src/context/types.js
+++ frontend/src/context/types.js
@@ -1,2 +1,4 @@
-export const LOGIN = "LOGIN";
-export const LOGOUT = "LOGOUT";
+export const USER_LOGIN = "USER_LOGIN";
+export const USER_LOGOUT = "USER_LOGOUT";
+export const GET_ALL_POSTS = "GET_ALL_POSTS";
+export const GET_PRIVATE_POSTS = "GET_PRIVATE_POSTS";
