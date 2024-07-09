b1b23fb659809328d7a99a7c79435845b29badab
Vinh Hoang <vinhhn3@fpt.edu.vn>
2024-07-09 08:46:28 +0700

    chore: Update App.js to include AuthState and PostState components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PostState from "./context/postContext/PostState";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthState>
+      <PostState>
+        <div>
+          <h1>Hello World</h1>
+        </div>
+      </PostState>
+    </AuthState>
   );
 }
 
